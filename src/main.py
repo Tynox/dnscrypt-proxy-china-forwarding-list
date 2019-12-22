414c4075e449ee951312295e3b9aa42e40fa6317
Alan Lyu <pocktynox@gmail.com>
2019-12-22 10:12:26 +0800

    first list

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ import requests
 import re
 
 
+# dns for china list
+DNS_RESOLVER = ["119.29.29.29", "223.5.5.5", "2400:3200:baba::1"]
+
+
 def fetch_china_list():
     url = 'https://github.com/felixonmars/dnsmasq-china-list/blob/master/accelerated-domains.china.conf?raw=true'
     r = requests.get(url)
@@ -14,14 +18,24 @@ def fetch_china_list():
 
 
 def parse_data(data):
-    server_re = '^server=\/(\S)+\/(\S)+$'
-
     servers = data.splitlines()
-    url_list = []
+    urls = []
     for server in servers:
-        print(server)
+        s = server.split("/")
+        urls.append(s[1])
+
+    return urls
+
+
+def write_to_file(urls):
+    with open("../forwarding_china_list.txt", "w") as f:
+        for url in urls:
+            dns = ",".join(DNS_RESOLVER)
+            line = "{0} {1}\n".format(url, dns)
+            f.write(line)
 
 
 if __name__ == "__main__":
     china_list = fetch_china_list()
-    parse_data(china_list)
+    url_list = parse_data(china_list)
+    write_to_file(url_list)
